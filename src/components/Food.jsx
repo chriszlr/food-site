ef7f6e5c7d4d03fdf2a0e5f4655b252dc54712fa
Christopher Zöllner <chriszoelln@gmail.com>
2023-05-13 12:15:13 +0200

    Food Component

---
+++ src/components/Food.jsx
@@ -0,0 +1,83 @@
+import { data } from "../data/data";
+import { useState } from "react";
+
+function Food() {
+  const [foods, setFoods] = useState(data);
+  return (
+    <div className="max-w-[1640px] mx-auto px-4 py-12">
+      <h1 className="text-orange-600 font-bold text-4xl text-center">
+        Top Rated Menu Items
+      </h1>
+
+      {/* Filter row */}
+      <div className="flex flex-col lg:flex-row justify-between">
+        {/* filter type */}
+        <div>
+          <p className="font-bold text-gray-700">Filter Type</p>
+          <div className="flex justify-between flex-wrap">
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              All
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              Burgers
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              Pizza
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              Salad
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              Chicken
+            </button>
+          </div>
+        </div>
+
+        {/* filter price */}
+        <div>
+          <p className="font-bold text-gray-700">Filter price</p>
+          <div className="flex justify-between max-w-[390px] w-full">
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              $
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              $$
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              $$$
+            </button>
+            <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white m-1">
+              $$$$
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* display foods */}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border rounded-lg shadow-lg hover:scale-105 duration-300"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-lg"
+            />
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white p-1 rounded-full">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Food;
